a266150846cd3edd0116b13e79c34c859fedb9b7
eco189 <eco189189@gmail.com>
2022-03-02 19:55:46 +0500

    Initial commit

--- project_1/game_v2.py
+++ project_1/game_v2.py
@@ -53,9 +53,7 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
 
-    return score
 
 
 if __name__ == "__main__":
-    # RUN
     score_game(random_predict)
